14c85a027626bd15fcccfd5b67e71e5599639017
Bilal Rauf <dark.flame282004@gmail.com>
2026-06-07 01:56:05 +0500

    Update main.py
    Added live link

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,7 @@ app = FastAPI(title="Hostel-Hub API", version="1.0.0", lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://hostel-hub-ten.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
